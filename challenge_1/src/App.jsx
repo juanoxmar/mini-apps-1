9aa7beaa4ca0217a1dd785c7338e8f0dfd3b3539
Juan Oxmar <joxmar.ramirez@gmail.com>
2021-01-04 10:57:24 -0800

    Setup babel, webpack, & react boilerplate

---
+++ challenge_1/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import classes from './App.module.css';
+
+export default function App() {
+  return (
+    <div className={classes.title}>
+      Did this render? Again
+    </div>
+  );
+}
